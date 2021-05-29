436f80938b1b4b9601dc9f9fe181fa18188d282a
Jai Kumar Dadhich <84243610+coderRj42@users.noreply.github.com>
2021-05-29 23:50:31 +0530

    Create toLowerCase .java

---
+++ day 11/toLowerCase .java
@@ -0,0 +1,33 @@
+/*
+Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+ 
+
+Example 1:
+
+Input: s = "Hello"
+Output: "hello"
+Example 2:
+
+Input: s = "here"
+Output: "here"
+Example 3:
+
+Input: s = "LOVELY"
+Output: "lovely"
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+s consists of printable ASCII characters.
+*/
+class Solution{  
+public String toLowerCase(String str) {
+        char[] a = str.toCharArray();
+        for (int i = 0; i < a.length; i++)
+            if ('A' <= a[i] && a[i] <= 'Z')
+                a[i] = (char) (a[i] - 'A' + 'a');
+        return new String(a);
+    }
+}
