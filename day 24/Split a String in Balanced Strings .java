e32dcdb394ca1b5aee37c3579323997e4abe8e6e
Jai Kumar Dadhich <84243610+coderRj42@users.noreply.github.com>
2021-06-11 23:48:21 +0530

    Create Split a String in Balanced Strings .java

---
+++ day 24/Split a String in Balanced Strings .java
@@ -0,0 +1,54 @@
+/*Balanced strings are those that have an equal quantity of 'L' and 'R' characters.
+
+Given a balanced string s, split it in the maximum amount of balanced strings.
+
+Return the maximum amount of split balanced strings.
+
+ 
+
+Example 1:
+
+Input: s = "RLRRLLRLRL"
+Output: 4
+Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+Example 2:
+
+Input: s = "RLLLLRRRLR"
+Output: 3
+Explanation: s can be split into "RL", "LLLRRR", "LR", each substring contains same number of 'L' and 'R'.
+Example 3:
+
+Input: s = "LLLLRRRR"
+Output: 1
+Explanation: s can be split into "LLLLRRRR".
+Example 4:
+
+Input: s = "RLRRRLLRLL"
+Output: 2
+Explanation: s can be split into "RL", "RRRLLRLL", since each substring contains an equal number of 'L' and 'R'
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s[i] is either 'L' or 'R'.
+s is a balanced string.*/
+
+class Solution {
+    public int balancedStringSplit(String s) {
+        int status=0;//what is the status of substring untill now
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='R'){
+                status++;
+            }else{
+                status--;
+            }
+            if(status==0){///when status is 0 -> means number of L and R are equal
+                count++;//count it in
+				//also status will be reset for next substring
+            }
+        }
+        return count;
+    }
+}
