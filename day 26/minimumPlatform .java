dde9e9c10c4a57d77642b44752881d1573797c73
Jai Kumar Dadhich <84243610+coderRj42@users.noreply.github.com>
2021-06-13 23:45:18 +0530

    Create minimumPlatform .java

---
+++ day 26/minimumPlatform .java
@@ -0,0 +1,29 @@
+
+class Solution
+{
+    static int findPlatform(int arr[], int dep[], int n)
+    {
+        Arrays.sort(arr);
+        Arrays.sort(dep);
+        
+        int i = 1, j = 0;
+        
+        int platform_needed = 1;
+        int res = 0;
+        while(i < n && j < n){
+            if(arr[i] <= dep[j]){
+                platform_needed++;
+                i++;
+            } else if(arr[i] > dep[j]){
+                platform_needed--;
+                j++;
+            }
+            
+            if(platform_needed > res)
+                res = platform_needed;
+        }
+        
+        return res;
+    }
+    
+}
